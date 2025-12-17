8f6a7e3ad916a796552381a5cd5eede0dd7d97b4
dell-kl <dennis-ponce07@outlook.com>
2025-12-17 11:49:04 -0500

    Cambios realizados

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.19")
+                .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
--- ServicioApiBodegaBalanceado/Program.cs
+++ ServicioApiBodegaBalanceado/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbSqlServerRemote"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbSqlServer"));
 });
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
